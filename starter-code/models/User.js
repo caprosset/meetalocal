e55dd1b5826923591ebe1bffba5608da9a6d053d
caprosset <capucinerosset@gmail.com>
2020-02-01 17:19:45 +0100

    Edit User model

--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -4,6 +4,14 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: String,
   password: String,
+  name: String,
+  surname: String,
+  countryOfOrigin: String,
+  age: Number,
+  countryOfInterest: String,
+  topicsOfInterest: String,
+  topicsOfExpertise: String,
+  profilePic: String
 });
 
 const User = mongoose.model('User', userSchema);
